066129a5e91e001139a5395df2fa71a6de42ffd0
Flynn <flynn@datawire.io>
2017-10-19 21:48:20 -0400

    Oops. Hopefully correct the kubernaut config.

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -5,10 +5,10 @@ set -e
 VERSION="$1"
 IMAGE="$2"
 
-echo "kubernaut set-token $(echo $KUBERNAUT_TOKEN | sum)"
-kubernaut set-token "$KUBERNAUT_TOKEN"
-
 set -x
+
+ls -l $HOME/.config/kubernaut
+sum $HOME/.config/kubernaut/config.json
 
 kubernaut claim 
 export KUBECONFIG=${HOME}/.kube/kubernaut
